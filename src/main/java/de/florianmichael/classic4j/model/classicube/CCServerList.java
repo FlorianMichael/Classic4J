02527ccb2606e66b6f37f690996df1c8952fb542
Luna <90072930+LunaWasFlaggedAgain@users.noreply.github.com>
2023-06-01 16:45:11 -0300

    Downgrade to Java 1.8 (#1)

--- src/main/java/de/florianmichael/classic4j/model/classicube/CCServerList.java
+++ src/main/java/de/florianmichael/classic4j/model/classicube/CCServerList.java
@@ -22,7 +22,16 @@ import de.florianmichael.classic4j.ClassiCubeHandler;
 
 import java.util.List;
 
-public record CCServerList(List<CCServerInfo> servers) {
+public class CCServerList {
+    private final List<CCServerInfo> servers;
+
+    public CCServerList(final List<CCServerInfo> servers) {
+        this.servers = servers;
+    }
+
+    public List<CCServerInfo> servers() {
+        return this.servers;
+    }
 
     public static CCServerList fromJson(final String json) {
         return ClassiCubeHandler.GSON.fromJson(json, CCServerList.class);
